40fc73aa979e979dec753291bee2ab0c4ae60a0b
Alexander <58190800+AlexanderPixel@users.noreply.github.com>
2022-01-07 15:07:24 +0200

    style: formatted tabs and spaces

--- Equation/Square Equation.hpp
+++ Equation/Square Equation.hpp
@@ -4,15 +4,15 @@
 
 class Square_Equation
 {
-    int a=0;
-    int b=0;
-    int c=0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
     
     double result1;
     double result2;
 public:
     Square_Equation(int a, int b, int c);
     void Count();
-    int getResult1()const;
-    int getResult2()const;
+    int getResult1() const;
+    int getResult2() const;
 };
