493dfa980f88e9e281f124483f1cd9fab8ba3306
Alexander <58190800+AlexanderPixel@users.noreply.github.com>
2022-01-07 15:06:03 +0200

    style: formatted tabs and spaces

--- Equation/Linear Equation.hpp
+++ Equation/Linear Equation.hpp
@@ -2,13 +2,13 @@
 
 #include "Equation.hpp"
 
-class Linear_Equation:public Equation
+class Linear_Equation: public Equation
 {
-    int result=0;
-    int a=0;
-    int b=0;
+    int result = 0;
+    int a = 0;
+    int b = 0;
 public:
     Linear_Equation(int a, int b);
-    int getResult()const;
+    int getResult() const;
     void Count();
 };
